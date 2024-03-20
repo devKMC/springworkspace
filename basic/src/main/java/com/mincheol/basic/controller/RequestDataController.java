604a1a2913f6a1d26f622ac1de99f14b31a8339a
devKMC <allcasa@naver.com>
2024-03-20 15:52:47 +0900

    2024-03-20 3:50

--- basic/src/main/java/com/mincheol/basic/controller/RequestDataController.java
+++ basic/src/main/java/com/mincheol/basic/controller/RequestDataController.java
@@ -1,6 +1,9 @@
 package com.mincheol.basic.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,8 +24,45 @@ public class RequestDataController {
     // http://localhost:4000/request-data/request-param?userid=qwer@userName=gildong
     public String getRequestParam(
         @RequestParam(name="userId") String userid,
-        @RequestParam(name="userName") String userName){
-        return "사용자 아이디 :" + userid + "/ 사용자 이름 :" + userName;
+        @RequestParam(name="userName" ,required = false) String userName,
+        @RequestParam() int userAge
+        ){
+        return "사용자 아이디 :" + userid + "/ 사용자 이름 :" + userName + "/ 사용자 나이 :" + userAge;
     }
 
+    // @pathVariable() : 
+    // 모든 HTTP 메서드에서 URL의 특정 패턴에 따라서 데이터를 추출하는 방식
+    // HTTP DELETE localhost:4000/request-data/path-variable
+    @DeleteMapping("/path-variable/{age}")
+    //http://localhost:4000/request-data/path-variable/10
+    public String deletePathVariable(
+        @PathVariable("age") Integer age
+    ){
+        return "사용자 나이 :" + age;
+    }
+
+    // HTTP PATCH localhost:4000/request-data/patch/gildong/update
+    @PatchMapping("/patch/{userName}/update")
+    public String patchUpdate(
+        @PathVariable("userName") String userName
+    ){
+        return "사용자이름: " + userName;
+    }
+
+    //! ************주의***************
+    // URL 패턴으로 데이터를 받아오는 방식을 썼을 때 
+    // 겹치는 패턴이 존재하는지 잘 확인해야 함
+    @GetMapping("/{value}/get")
+    public String getPathVariable1(
+        @PathVariable("value") String value
+    ){
+        return "";
+    }
+
+    @GetMapping("/get/{value}")
+    public String getPathVariable2(
+        @PathVariable("value") String value
+    ){
+        return "";
+    }
 }
