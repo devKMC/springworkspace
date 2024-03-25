0f8515de43257379418ba9f3873f7b0f4290606e
devKMC <allcasa@naver.com>
2024-03-25 14:51:21 +0900

    2024-03-25 2:50

---
+++ basic/src/main/java/com/mincheol/basic/service/implement/BasicServiceImplement.java
@@ -0,0 +1,12 @@
+package com.mincheol.basic.service.implement;
+
+import com.mincheol.basic.service.BasicService;
+
+public class BasicServiceImplement implements BasicService{
+
+    @Override
+    public String getHello() {
+        return "hello Springboot!!";
+    }
+    
+}
