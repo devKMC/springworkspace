2db439876ada91c13cdaef1adfbe1531ab237a47
devKMC <allcasa@naver.com>
2024-03-25 15:51:04 +0900

    2024-03-25 3:50

--- basic/src/main/java/com/mincheol/basic/controller/BasicController.java
+++ basic/src/main/java/com/mincheol/basic/controller/BasicController.java
@@ -1,5 +1,6 @@
 package com.mincheol.basic.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -8,6 +9,11 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mincheol.basic.service.BasicService;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 //Controller 레이어 :
 // - 클라이언트의 서버간의 접점
@@ -23,7 +29,17 @@ import org.springframework.web.bind.annotation.RestController;
 // @RequestMapping(value ="/main", method= {RequestMethod.GET})
 // HTTP GET localhost:4000/main/**
 @RequestMapping("/main")
+@RequiredArgsConstructor
 public class BasicController {
+
+    // private BasicService service;
+
+    // @autowirde
+    // public BasicController (BasicService service){
+    //     this.service = service;
+    // }
+
+    private final BasicService service;
 
     //HTTP GET localhost:4000/main/  <-- 까지 요청이 밑에 코드
     @RequestMapping(value="/", method = {RequestMethod.GET})
--- basic/src/main/java/com/mincheol/basic/service/implement/BasicServiceImplement.java
+++ basic/src/main/java/com/mincheol/basic/service/implement/BasicServiceImplement.java
@@ -1,12 +1,24 @@
 package com.mincheol.basic.service.implement;
 
+import org.springframework.stereotype.Service;
+
 import com.mincheol.basic.service.BasicService;
 
+// Service 레이어 :
+// - 실제 비즈니스 로직(연산)을 실행하는 영역
+// - 트랜젝션 처리나 유효성 검사(직전 비즈니스 로직 결과에 대한)를 수행하기도 함
+// Controller로부터 받은 요청 데이터에 대해서 필요에 따라 Repository 등을 거쳐 연산을 진행하고 Controller에 응답에 대한 데이터를 반환
+@Service
 public class BasicServiceImplement implements BasicService{
 
     @Override
     public String getHello() {
         return "hello Springboot!!";
+    }
+
+    @Override
+    public String getApple() {
+        return "get Mapping으로 만든 메서드";
     }
     
 }
